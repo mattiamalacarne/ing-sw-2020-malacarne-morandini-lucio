aed782cda29466584359db13f339b363b37d810d
Mattia Malacarne <malacarne.mattia97@gmail.com>
2020-06-17 16:00:45 +0200

    Added message screen on the GUI

--- src/main/java/it/polimi/ingsw/psp12/view/userinterface/GUI/screens/SetUpUtils/GenericMessageScreen.java
+++ src/main/java/it/polimi/ingsw/psp12/view/userinterface/GUI/screens/SetUpUtils/GenericMessageScreen.java
@@ -8,17 +8,34 @@ import java.awt.*;
  * @author Mattia Malacarne
  */
 
-public class GenericMessageScreen extends JPanel
+public class GenericMessageScreen extends JLayeredPane
 {
     private JLabel waitText;
+    private JLabel backg;
 
     public GenericMessageScreen(Dimension size, String text) {
 
-        waitText = new JLabel(text);
+        Image bg = loadScreenBackground("firstscreen.png", new Dimension(size.width, size.height));
+        backg = new JLabel(new ImageIcon(bg));
+
+        backg.setBounds(0,0, size.width, size.height);
+
+        waitText = new JLabel(text, SwingConstants.CENTER);
+        waitText.setFont(new Font("dalek", Font.PLAIN, 28));
+        waitText.setBounds(490,50, 437, 300);
+
+        this.setLayout(null);
 
         this.setBounds(0,0, size.width,size.height);
-        this.add(waitText);
+        this.add(waitText, JLayeredPane.DRAG_LAYER);
+        this.add(backg, JLayeredPane.DEFAULT_LAYER);
         this.setVisible(true);
         this.setVisible(true);
+    }
+
+    public Image loadScreenBackground(String bg, Dimension size) {
+        ImageIcon icon = new ImageIcon(getClass().getResource("/"+bg));
+        Image scaled = icon.getImage().getScaledInstance(size.width, size.height, Image.SCALE_SMOOTH);
+        return scaled;
     }
 }
